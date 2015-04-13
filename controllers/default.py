8c97d04c08cc7cf603f527db095247f8ee2851e0
sigrond <sigrond93@gmail.com>
2015-04-13 18:05:57 +0200

    Revert "Działający szkic"
    This reverts commit 578d1466b473f2afa2d19f4b0685c611cae2e313.

--- controllers/default.py
+++ controllers/default.py
@@ -26,7 +26,6 @@ def get_field():
 		game = new_game()
 	else:
 		if shoot_field != None:
-			#my strzelamy
 			shoot_field = int(shoot_field)
 			if shoot_field < 0 or shoot_field > 99:
 				return "Niepoprawne Dane!"
@@ -34,24 +33,10 @@ def get_field():
 			if field=="w":
 				print("pudło")
 				session.cpu_field[shoot_field] = "p"
+			#trafiony
 			if field=="s": 
 				print("trafiony")
 				session.cpu_field[shoot_field] = "t"
-			#komputer strzela
-			#trzeba przerobić na stringa. Nie wiem. Może przyjmuje tablice, albo listy?
-			cpu_shoot = ai.shoot("".join(session.player_field))
-			field = session.player_field[cpu_shoot]
-			if field=="w":
-				session.player_field[cpu_shoot] = "p"
-			if field=="s": 
-				session.player_field[cpu_shoot] = "t"
-			#sprawdzamy, kto wygrał, to zwracanie stringa zamiast strony trzeba będzie jeszcze dopracować
-			if session.player_field.count("s") == 0 and session.cpu_field.find("s") == -1:
-				return "Remis!"
-			elif session.player_field.count("s") == 0:
-				return "Komputer wygrał!"
-			elif session.cpu_field.count("s") == 0:
-				return "Gracz wygrał!"
 		game = continue_game()
 	return game
 
