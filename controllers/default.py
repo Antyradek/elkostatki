2a87cb981035edb6de3f749c546eb1aee1753b44
Radosław Świątkiewicz <antyradek@gmail.com>
2015-06-09 15:22:00 +0200

    Kilka komentarzy

--- controllers/default.py
+++ controllers/default.py
@@ -1,10 +1,11 @@
+## Wczytywanie C++
 ai = local_import("elkostatki_ai", reload = True);
 
 ## Główna otoczka do gry
 def index():
 	return dict()
 
-#ustaw nową grę, wygeneruj i ustaw do sesji
+# Ustaw nową grę, wygeneruj i ustaw do sesji plansze
 def new_game():
 	player_field = ai.generate_field()
 	cpu_field = ai.generate_field()
@@ -12,22 +13,23 @@ def new_game():
 	session.cpu_field = list(cpu_field)
 	return dict(player_field=player_field, cpu_field=cpu_field)
 	
+## Odczytaj już istniejące plansze
 def continue_game():
 	return dict(player_field=session.player_field, cpu_field=session.cpu_field)
 
 ## Pobierz pole z grą
 def get_field():
-	#jeśli wymusimy nową planszę 
+	#jeśli wymusimy nową planszę, (wartością GET) 
 	force_new_game = request.vars["force_new_game"];
-	#jeśli oddajemy strzał przy okazji
+	#jeśli oddajemy strzał gracza
 	shoot_field = request.vars["shoot"];
 	game = dict()
 	if not session.player_field or not session.cpu_field or force_new_game == "true":
-		#nowy użytkownik
+		#jeśli sesja jest pusta. generujemy nową
 		game = new_game()
 	else:
 		if shoot_field != None:
-			#my strzelamy
+			#gracz strzela
 			shoot_field = int(shoot_field)
 			if shoot_field < 0 or shoot_field > 99:
 				return "Niepoprawne Dane!"
@@ -39,14 +41,14 @@ def get_field():
 				print("trafiony")
 				session.cpu_field[shoot_field] = "t"
 			#komputer strzela
-			#trzeba przerobić na stringa. Nie wiem. Może przyjmuje tablice, albo listy?
+			#odwołanie do C++
 			cpu_shoot = ai.shoot("".join(session.player_field))
 			field = session.player_field[cpu_shoot]
 			if field=="w":
 				session.player_field[cpu_shoot] = "p"
 			if field=="s": 
 				session.player_field[cpu_shoot] = "t"
-			#sprawdzamy, kto wygrał, to zwracanie stringa zamiast strony trzeba będzie jeszcze dopracować
+			#sprawdzamy, kto wygrał
 			if session.player_field.count("s") == 0 and session.cpu_field.find("s") == -1:
 				return "<h1 class='win'>Remis!</h1>"
 			elif session.player_field.count("s") == 0:
